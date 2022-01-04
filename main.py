6cefc11f6b35ccce48a0389b3546c789f8550c63
Vishnu Ajith <27vishnu07@gmail.com>
2022-01-04 22:45:01 +0530

    updated

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ print(geocoder.description_for_number(ch_number,"en"))
 from phonenumbers import carrier
 ph_number = phonenumbers.parse(number,"RO")
 print(carrier.name_for_number(ph_number,"en"))
+
+from phonenumbers import timezone as tz
+tz_number = phonenumbers.parse(number)  
+print(tz.time_zones_for_number(tz_number))
